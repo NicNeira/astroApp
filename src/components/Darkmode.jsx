c29c0a85e4896b7a21034f50045c8f4156f35083
NicNeira <neiranicolas17@gmail.com>
2022-12-21 15:54:51 -0300

    fixed class to className and add button for darkmode

--- src/components/Darkmode.jsx
+++ src/components/Darkmode.jsx
@@ -2,62 +2,86 @@
   /* darkmode switch */
 }
 import React, { useEffect, useState } from "react";
+import ReactSwitch from "react-switch";
 
 export const Darkmode = () => {
-  const [darkToggle, setDarkToggle] = useState("light");
+  const [checked, setChecked] = useState(false);
+  const root = window.document.documentElement;
+  const lightTheme = "light";
+  const darkTheme = "dark";
+  let theme;
 
   useEffect(() => {
-    if (darkToggle === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
+    if (localStorage) {
+      theme = localStorage.getItem("theme");
     }
-  }, [darkToggle]);
+    if (theme === lightTheme || theme === darkTheme) {
+      root.classList.add(theme);
+    } else {
+      root.classList.add(lightTheme);
+    }
+  }, [checked]);
 
-  const handleDarkMode = () => {
-    setDarkToggle(darkToggle === "dark" ? "light" : "dark");
+  const handleChange = () => {
+    if (theme === darkTheme) {
+      root.classList.replace(darkTheme, lightTheme);
+      localStorage.setItem("theme", "light");
+      theme = lightTheme;
+      setChecked(false);
+    } else {
+      root.classList.replace(lightTheme, darkTheme);
+      localStorage.setItem("theme", "dark");
+      setChecked(true);
+    }
   };
+
+  console.log(localStorage.getItem("theme"));
 
   return (
     <div className="flex items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 dark:text-white"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-        />
-      </svg>
+      <button onClick={handleChange}>
+        {checked ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 dark:text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-5 h-5 dark:text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        )}
+      </button>
 
-      <label class="inline-flex relative  cursor-pointer mx-2">
+      {/* <label className="inline-flex relative  cursor-pointer mx-2">
         <input
           onChange={handleDarkMode}
           type="checkbox"
           value=""
-          class="sr-only peer"
+          className="sr-only peer"
         />
-        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-      </label>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-5 h-5 dark:text-white"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-        />
-      </svg>
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      </label> */}
     </div>
   );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,13 @@ export const Footer = () => {
   return (
     <div className=" mx-auto max-w-6xl px-8 mt-10">
       <div className="flex justify-center">
-        <a href="#!" class="mr-9 text-gray-800">
+        <a href="#!" className="mr-9 text-gray-800">
           <svg
             aria-hidden="true"
             focusable="false"
             data-prefix="fab"
             data-icon="linkedin-in"
-            class="dark:text-white svg-inline--fa fa-linkedin-in w-7 h-7"
+            className="dark:text-white svg-inline--fa fa-linkedin-in w-7 h-7"
             role="img"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512"
@@ -21,13 +21,13 @@ export const Footer = () => {
             ></path>
           </svg>
         </a>
-        <a href="#!" class="text-gray-800">
+        <a href="#!" className="text-gray-800">
           <svg
             aria-hidden="true"
             focusable="false"
             data-prefix="fab"
             data-icon="github"
-            class="dark:text-white svg-inline--fa fa-github w-7 h-7"
+            className="dark:text-white svg-inline--fa fa-github w-7 h-7"
             role="img"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 496 512"
@@ -46,13 +46,13 @@ export const Footer = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-10 h-10 text-blue-700"
+            className="w-10 h-10 text-blue-700"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
             />
           </svg>
